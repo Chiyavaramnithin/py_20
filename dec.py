e04134fe73a0db20ba96359c9dd5c85b27e163b9
nithin <nithinchiyavaram@gmail.com>
2026-08-17 16:12:22 +0530

    py20

--- dec.py
+++ dec.py
@@ -33,3 +33,31 @@ double=lambda x:x*2
 print(square(7))
 print(cube(7))
 print(double(7))
+
+''' . Create a function process_marks(marks, operation) where operation is a function reference.
+
+Use lambda functions to:
+
+* Add 5 grace marks
+* Double each mark
+* Find whether a mark is greater than 40
+
+Create a decorator that prints "Processing started" and "Processing completed".'''
+
+def ad(func):
+    def wrapper(*args,**kwargs):
+        print("Processing Started")
+        result=func(*args,**kwargs)
+        print("Processing completed")
+        return result
+    return wrapper
+@ad
+def process_marks(marks,operation):
+    return list(map(operation,marks))
+marks=[40,45,60,32]
+add = lambda x: x + 5
+double = lambda x: x * 2
+greater = lambda x: x > 40
+print("Add Grace Marks:", process_marks(marks, add))
+print("Double Marks:", process_marks(marks, double))
+print("Greater than 40:", process_marks(marks, greater))
