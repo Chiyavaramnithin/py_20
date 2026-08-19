f483e9e6c84b20c75c2c4a83f4a39d110d6074cd
nithin <nithinchiyavaram@gmail.com>
2026-08-19 09:19:37 +0530

    py20

--- usingLEGB.py
+++ usingLEGB.py
@@ -17,3 +17,25 @@ for i in range(3):
         break
 else:
     print("Account is locked")
+'''A system wants to track the number of times the login() function is called. Create a global variable attempts = 0
+ and a decorator track_attempts that increments the global variable every time the login function is executed.
+The login() function should accept username and password as parameters and display "Login attempted by <username>".
+Call the function three times with different usernames and passwords, and finally display the total number of login
+attempts. Use the global keyword inside the decorator to modify the global variable.'''
+
+attempts=0
+def login(func):
+    def wrapper(login,password,*args,**kwargs):
+        global attempts
+        attempts+=1
+        return func(login,password,*args,**kwargs)
+    return wrapper
+@login
+def logins(username,password):
+    print("username:",username)
+    print("password",password)
+logins("nithin","nithin@1234")
+logins("trinesh","trinesh@1234")
+logins("nikhil","nikhil@1234")
+print("total no of attempts: ",attempts)
+
