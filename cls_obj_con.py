25aa926b10ae468e642259e21756d36c20618c38
nithin <nithinchiyavaram@gmail.com>
2026-08-26 11:39:49 +0530

    py20

---
+++ cls_obj_con.py
@@ -0,0 +1,30 @@
+class A:
+    pass
+obj1=A()
+print(type(obj1))
+obj2=A()
+print(id(obj1))
+print(id(obj2))
+
+class phone:
+    software='android'
+    count=0
+    def __init__(self,brand,price):
+        self.brand=brand
+        self.price=price
+        phone.count+=1
+phone1=phone('samsung','100000')
+phone2=phone('Iqoo','30000')
+print(phone1.brand)
+print(phone1.price)
+print(phone2.brand)
+print(phone2.price)
+print(phone.count) # for identifing the total no of phone
+
+class A:
+    x=20
+    def __init__(self):
+        self.y=120
+        self.x=40
+        self.a=140
+obj=A()
