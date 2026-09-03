9206072e6c99736e93dc8040575ad3bf15857488
nithin <nithinchiyavaram@gmail.com>
2026-09-03 12:37:55 +0530

    py20

--- cls_obj_con.py
+++ cls_obj_con.py
@@ -20,6 +20,22 @@ print(phone1.price)
 print(phone2.brand)
 print(phone2.price)
 print(phone.count) # for identifing the total no of phone
+class A:
+    x=20
+    c=0
+    def __init__(self):
+        self.y=120
+        self.x=40
+        self.a=140
+        A.c+=1
+obj=A()
+obj2=A()
+print(obj.y)
+print(obj.x)
+print(obj.a)
+print(obj.y)
+print(obj.y)
+print(obj2.c)
 
 '''Create a class "zomato" with attributes like class variables: discount, coupon code and a list of all the restuarant
 names, instance attributes: restaurant name,a dict of items, restuarant id, calculate restuarant id based on another 
@@ -30,7 +46,7 @@ class zomato:
     discount = 30
     coupon_code = "ZOMATO30"
     restaurant_names = []
-    restaurant_number = 100
+    restaurant_number = 630
     def __init__(self, restaurant_name, items):
         self.restaurant_name = restaurant_name
         self.items = items
@@ -58,22 +74,7 @@ print("Items:", r1.items)
 item_no = int(input("Enter item number: "))
 r1.order(item_no)
 
-class A:
-    x=20
-    c=0
-    def __init__(self):
-        self.y=120
-        self.x=40
-        self.a=140
-        A.c+=1
-obj=A()
-obj2=A()
-print(obj.y)
-print(obj.x)
-print(obj.a)
-print(obj.y)
-print(obj.y)
-print(obj2.c)
+
 
 class A:
     def __init__(self):
