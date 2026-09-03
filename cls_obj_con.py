af228ce4d09a19ea1d97bbd727904e68d5635b6c
nithin <nithinchiyavaram@gmail.com>
2026-09-03 12:36:05 +0530

    py20

--- cls_obj_con.py
+++ cls_obj_con.py
@@ -21,6 +21,43 @@ print(phone2.brand)
 print(phone2.price)
 print(phone.count) # for identifing the total no of phone
 
+'''Create a class "zomato" with attributes like class variables: discount, coupon code and a list of all the restuarant
+names, instance attributes: restaurant name,a dict of items, restuarant id, calculate restuarant id based on another 
+class variable restuarant number,Create a function order which takes an item_no as input and checks wheather the user
+ as a entered valid item and prints final_bill,if enter coupon code correctly with discount'''
+
+class zomato:
+    discount = 30
+    coupon_code = "ZOMATO30"
+    restaurant_names = []
+    restaurant_number = 100
+    def __init__(self, restaurant_name, items):
+        self.restaurant_name = restaurant_name
+        self.items = items
+        zomato.restaurant_number += 1
+        self.restaurant_id = zomato.restaurant_number
+        zomato.restaurant_names.append(restaurant_name)
+    def order(self, item_no):
+        if item_no in self.items:
+            bill = self.items[item_no]
+            coupon = input("Enter coupon code: ")
+            if coupon == zomato.coupon_code:
+                discount_amount = bill * zomato.discount / 100
+                final_bill = bill - discount_amount
+                print("Final bill:", final_bill)
+            else:
+                print("Invalid coupon code")
+                print("Final bill:", bill)
+        else:
+            print("Invalid item number")
+r1 = zomato("Paradise", {1: 300, 2: 280, 3: 520})
+r2 = zomato("Mehfil", {1: 340, 2: 550, 3: 480})
+print("Restaurant Name:", r1.restaurant_name)
+print("Restaurant ID:", r1.restaurant_id)
+print("Items:", r1.items)
+item_no = int(input("Enter item number: "))
+r1.order(item_no)
+
 class A:
     x=20
     c=0
@@ -64,4 +101,3 @@ class A:
             print(name)
         else:
             print("Invalid")
-p=A(22,"nithin")
